dd7b7af163062f43ca4e40b156a08495a318396f
tonnitikorn1 <c.nitikorn@softkingdoms.com>
2023-02-14 12:27:04 +0700

    line login

--- pages/auth/callback.js
+++ pages/auth/callback.js
@@ -10,8 +10,12 @@ function Callback() {
     const [loading, setLoading] = useState(false)
     const router = useRouter()
     const { code, state } = router.query
-
-    // console.log('code', code)
+    let token;
+    const ISSERVER = typeof window === "undefined";
+    if (!ISSERVER) {
+        token = new URLSearchParams(window.location.search).get("code");
+    }
+    // console.log('token', token)
 
     // if (code) {
     //     console.log('if')
@@ -22,6 +26,7 @@ function Callback() {
 
     const runApp = async () => {
         console.log('code', code)
+        console.log('token', token)
         // if (!code) {
         //     console.log('if')
         //     setLoading(false)
@@ -30,9 +35,9 @@ function Callback() {
             console.log('code else', code)
 
             let res = await axios({
-                headers: {
-                    Authorization: "Bearer " + localStorage.getItem("access_token"),
-                },
+                // headers: {
+                //     Authorization: "Bearer " + localStorage.getItem("access_token"),
+                // },
                 method: "post",
                 url: `${hostname}/auth/login_line`,
                 data: {
