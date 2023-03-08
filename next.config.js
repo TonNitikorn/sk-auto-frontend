193be28d3212ebc7cd67bcdac13bb8b7a61612c3
tonnitikorn1 <c.nitikorn@softkingdoms.com>
2023-03-08 13:43:35 +0700

    update member 90%

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
