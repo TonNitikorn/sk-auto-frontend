7293ca10c1a7a4ec4ca56fa6aa65d3ee5f8dac5a
tonnitikorn1 <c.nitikorn@softkingdoms.com>
2023-03-08 13:39:03 +0700

    update member 90%

--- components/page/withdraw.js
+++ components/page/withdraw.js
@@ -95,62 +95,7 @@ function WithdrawComponent() {
 
     return (
         <Box sx={{ m: 2, mb: 15 }}>
-            {/* <Typography variant="h6" sx={{ fontWeight: 'bold', mt: 3 }}>ถอนเครดิต</Typography>
-            <Typography sx={{ fontSize: '12px' }}>เลือกจำนวนเครดิต</Typography>
-
-            <Grid container spacing={2} sx={{ mt: 2 }}>
-                {amount.map((item) => (
-                    <>
-                        <Grid item xs={3}>
-                            <Button
-                                variant="contained"
-                                fullWidth
-                                sx={{
-                                    p: 1,
-                                    bgcolor: '#0072B1'
-                                }}
-                                onClick={() => setPrice(item)}
-                            >
-                                <Typography
-                                    sx={{ textAlign: "center", color: "#fff" }}
-                                >
-                                    {Intl.NumberFormat("THB").format(item)}
-                                </Typography>
-                            </Button>
-                        </Grid>
-                    </>
-                ))}
-            </Grid>
-
-            <Typography sx={{ mt: 4, mb: 2 }}>
-                ระบุจำนวนที่ต้องการถอน
-            </Typography>
-            <TextField
-                name="code"
-                type="number"
-                fullWidth
-                value={price || ""}
-                sx={{ bgcolor: "#fff", borderRadius: 2, }}
-                onChange={(e) => setPrice(e.target.value)}
-            />
-            <Typography sx={{ color: "#aaa", mt: 1, mb: 2, fontSize: "12px" }}>
-                ถอนไม่มีขั้นต่ำ
-            </Typography>
-
-            <Button
-                variant="contained"
-                size="large"
-                fullWidth
-                onClick={() => handelwithdraw()}
-                sx={{
-                    mt: 5,
-                    bgcolor: "#41A3E3",
-                    color: 'white'
-                }}
-            >
-                ถอนเงิน
-
-            </Button> */}
+           
             <LoadingModal open={loading} />
 
         </Box>
