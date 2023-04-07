2f3d06fd6bdab75aaba7debe5d88e3250b02f1e9
tonnitikorn1 <c.nitikorn@softkingdoms.com>
2023-04-07 13:06:09 +0700

    game test

--- components/page/home.js
+++ components/page/home.js
@@ -376,19 +376,29 @@ function Home() {
                         <Typography sx={{ fontSize: '14px', mt: 1 }} onClick={() => setSelectGame(false)}  >ย้อนกลับ</Typography>
                     </Grid>
                     <Grid container spacing={1} sx={{ p: 1, textAlign: "center" }}>
-                        {gameType.map((item) => (
+                        <Grid item xs={6} md={3}>
+                            <img src={`https://angpaos.games/wp-content/uploads/2023/04/Banner-เกมส์กัญชา.jpg`} width={'100%'} height={100} style={{ borderRadius: '10px' }}
+                                onClick={() => window.location.href = `https://sk-dev-test-app-9ood3.ondigitalocean.app/?token=${localStorage.getItem('access_token')}`}
+                            />
+                        </Grid>
+                        <Grid item xs={6} md={3}>
+                            <img src={'https://angpaos.games/wp-content/uploads/2023/04/Banner-เกมส์หมู.jpg'} width={'100%'} height={100} style={{ borderRadius: '10px' }} />
+                        </Grid>  <Grid item xs={6} md={3}>
+                            <img src={'https://angpaos.games/wp-content/uploads/2023/04/Banner-เกมส์ควาย.jpg'} width={'100%'} height={100} style={{ borderRadius: '10px' }} />
+                        </Grid>
+                        {/* {gameType.map((item) => (
                             <>
-                                {/* <Button
-                           sx={{ mt: 1, height: '80px', width: '90%', borderRadius: '20px' }}
-                           onClick={() => {
-                              console.log('item.sub_game_type', item.sub_game_type)
-                              setSubGameType(item.sub_game_type)
-                           }}
-                        >
-                           <Box >
-                              <img src={item.type_logo} width={80} height={80} style={{ borderRadius: '20px' }} />
-                           </Box>
-                        </Button> */}
+                                <Button
+                                    sx={{ mt: 1, height: '80px', width: '90%', borderRadius: '20px' }}
+                                    onClick={() => {
+                                        console.log('item.sub_game_type', item.sub_game_type)
+                                        setSubGameType(item.sub_game_type)
+                                    }}
+                                >
+                                    <Box >
+                                        <img src={item.type_logo} width={80} height={80} style={{ borderRadius: '20px' }} />
+                                    </Box>
+                                </Button>
                                 <Grid item xs={6} md={3}>
                                     <img src={item.type_logo} width={80} height={80} style={{ borderRadius: '20px' }} />
 
@@ -396,7 +406,7 @@ function Home() {
 
 
                             </>
-                        ))}
+                        ))} */}
                     </Grid>
                 </Box>
             }
